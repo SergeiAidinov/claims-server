f15e8c108f2c7943055ecd1ed3d21f79808b8e2e
SergeiAidinov <incoming34@yandex.ru>
2024-03-09 14:30:45 +0300

    Starts

---
+++ src/main/java/ru/yandex/incoming34/server/ServerJwtApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.incoming34.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerJwtApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/yandex/incoming34/server/domain/RefreshJwtRequest.java
@@ -0,0 +1,12 @@
+package ru.yandex.incoming34.server.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RefreshJwtRequest {
+
+    public String refreshToken;
+
+}
