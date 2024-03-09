2764a4e49ec6491e687f69606610ddaf8449e15a
SergeiAidinov <incoming34@yandex.ru>
2024-03-09 16:19:56 +0300

    Enabled swagger

---
+++ src/main/java/ru/yandex/incoming34/server/config/OpenApiConfig.java
@@ -0,0 +1,59 @@
+package ru.yandex.incoming34.server.config;
+
+import java.io.File;
+import java.util.Objects;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.jsonschema.JsonSchema;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@SpringBootConfiguration
+@EnableOpenApi
+@ComponentScan(basePackageClasses = { ru.yandex.incoming34.server.controller.Controller.class })
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenApi() {
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.setInfo(info());
+        return openAPI;
+    }
+
+    @Bean
+    Info info() {
+        return new Info().title("Приложение для запомнинаия спряжения неправильных глаголов французского языка")
+                .description("Pet project").version(componentVersion()).contact(contact());
+    }
+
+    @Bean
+    Contact contact() {
+        return new Contact().email("incoming34@yandex.ru").name("Sergei Aidinov");
+    }
+
+    @SuppressWarnings("deprecation")
+    private String componentVersion() {
+        String propertiesFileName = "pom.xml";
+        String componentVersion = "Версия не указана";
+        File file = new File(propertiesFileName);
+        XmlMapper xmlMapper = new XmlMapper();
+        try {
+            JsonSchema jsonSchema = xmlMapper.generateJsonSchema(String.class);
+            JsonSchema json = xmlMapper.readValue(file, jsonSchema.getClass());
+            componentVersion = Objects.nonNull(json.getSchemaNode().get("version"))
+                    ? String.valueOf(json.getSchemaNode().get("version")).replaceAll("\"", "")
+                    : componentVersion;
+        } catch (Exception e) {
+            return componentVersion;
+        }
+        return componentVersion;
+    }
+
+}
--- src/main/java/ru/yandex/incoming34/server/config/SecurityConfig.java
+++ src/main/java/ru/yandex/incoming34/server/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package ru.yandex.incoming34.server.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.yandex.incoming34.server.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -12,12 +17,26 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig /*extends WebSecurityConfigurerAdapter */{
 
     private final JwtFilter jwtFilter;
+    /*@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(encoder.encode("admin"))
+                .roles("USER", "ADMIN");
+    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -34,5 +53,4 @@ public class SecurityConfig {
                                 .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 ).build();
     }
-
 }
